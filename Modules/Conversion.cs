e8cd3cc8a57ac53f2349bf90257d8076c204f3a6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-07 16:13:27 +0530

    Modules: Conversion

---
+++ Modules/Conversion.cs
@@ -0,0 +1,11 @@
+/**
+ * @file Conversion.cs
+ * @author Krisna Pranav
+ * @brief Conversion
+ * @version 6.0
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2022-2024 dynutOS Developers, Krisna Pranav
+ * 
+ */
+ 
