b0e64ea5d97f0b339f384bf73ca295a420a5b6a2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-07 16:17:53 +0530

    Modules: Conversion

--- Modules/Conversion.cs
+++ Modules/Conversion.cs
@@ -47,5 +47,28 @@ namespace DynutOS.Modules
                 return text;
             }
         }
+
+        public static string Hex(this byte[] value)
+        {
+            int offset = 0;
+            int length = -1;
+
+            if (length < 0) {
+                length = value.Length - offset;
+            }
+
+            var builder = new StringBuilder(length * 2);
+            int b;
+
+            for(int i = offset; i < length + offset; i++) {
+                b = value[i] >> 4;
+                builder.Append((char)(55 + b + (((b - 10) >> 31) & -7)));
+                b = value[i] & 0xF;
+                builder.Append((char)(55 + b + (((b - 10) >> 31) & -7)));
+
+            }
+            return builder.ToString();
+        }
+
     } // public static class Conversion
 } // namespace DynutOS.Modules
