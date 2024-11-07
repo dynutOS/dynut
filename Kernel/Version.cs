cb226a4fe22a58fa7f4995b1f52422a60f279a95
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-07 16:13:15 +0530

    Kernel: Version

--- Kernel/Version.cs
+++ Kernel/Version.cs
@@ -1,3 +1,14 @@
+/**
+ * @file Version.cs
+ * @author Krisna Pranav
+ * @brief Version
+ * @version 6.0
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2022-2024 dynutOS Developers, Krisna Pranav
+ * 
+ */
+
 namespace DynutOS 
 {
     public class VersionInfo
