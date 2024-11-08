6db0a24ed7ed2d2921f174e32a83a9fbcec1ccfa
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-08 17:06:57 +0530

    Modules: Misc

---
+++ Modules/Misc.cs
@@ -0,0 +1,33 @@
+/**
+ * @file Misc.cs
+ * @author Krisna Pranav
+ * @brief Misc
+ * @version 6.0
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2022-2024 dynutOS Developers, Krisna Pranav
+ * 
+ */
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynutOS.System.Utils
+{
+    public class Misc
+    {
+        public static bool IsIPv4Address(string[] items)
+        {
+            if (items.length == 4) {
+                int n0;
+                if (int.TryParse(items[0], out n0) && int.TryParse(items[1], out n0) && int.TryParse(items[2], out n0) && int.TryParse(items[3], out n0)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    } // public class Misc
+} // namespace DynutOS.System.Utils
