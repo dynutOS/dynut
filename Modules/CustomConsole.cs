d48a15ff4ab9fd7683abb119574852f515a5e887
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-11-08 17:00:00 +0530

    Modules: CustomConsole

---
+++ Modules/CustomConsole.cs
@@ -0,0 +1,39 @@
+/**
+ * @file CustomConsole.cs
+ * @author Krisna Pranav
+ * @brief Custom Console
+ * @version 6.0
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2022-2024 dynutOS Developers, Krisna Pranav
+ * 
+ */
+
+using System;
+
+namespace DynutOS.System
+{
+    public class CustomConsole
+    {
+        public static void WriteLineInfo(string text)
+        {
+            if (BootConsole != null) {
+                BootConsole.Foreground = ConsoleColor.Cyan;
+                BootConsole.Write("[Info] ");
+                BootConsole.Foreground = ConsoleColor.White;
+                BootConsole.Write(text + "\n");
+                BootConsole.Draw();
+                Kernel.Canvas.DrawImage(BootConsole.GetBuffer(), 0, 0);
+                Kernel.Canvas.Display();
+            } else {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("[Info] ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(text + "\n");
+            }
+
+            Logs.DoOSLog("[Info] " + text);
+        }
+        
+    } // public class CustomConsole
+} // namespace DynutOS.System
